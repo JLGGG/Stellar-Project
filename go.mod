22b76f98a77787e66821d0001c38eb0c3ecab5ad
Junsu Lim <blockboy2957@gmail.com>
2021-03-09 22:44:56 +0900

    Create a create_keypair.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JLGGG/Stellar-Project
+
+go 1.16
+
+require github.com/stellar/go v0.0.0-20210308205532-bdfa690e1546 // indirect
---
+++ src/create_keypair.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/stellar/go/keypair"
+)
+
+func main() {
+	pair, err := keypair.Random()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(pair.Seed())
+	// SAV76USXIJOBMEQXPANUOQM6F5LIOTLPDIDVRJBFFE2MDJXG24TAPUU7
+	log.Println(pair.Address())
+	// GCFXHS4GXL6BVUCXBWXGTITROWLVYXQKQLF4YH5O5JT3YZXCYPAFBJZB
+}
