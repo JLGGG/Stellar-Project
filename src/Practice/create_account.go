9dd0cf76c26e357094d03006494baa01bfee677c
Junsu Lim <blockboy2957@gmail.com>
2021-03-11 20:44:17 +0900

    Move src to practice

--- src/Practice/create_account.go
+++ src/Practice/create_account.go
@@ -20,6 +20,11 @@ func main() {
 	seed := pair.Seed()
 	log.Printf("Secret key: %s", seed)
 	log.Printf("Public key: %s", address)
+	//2021/03/10 07:28:45 Secret key: SBCRHU24LEAUGZ7PMFAXNPCZM34JGGYD2N3CPILQIGQWBZFNS7VOCDZ6
+	//2021/03/10 07:28:45 Public key: GA7KLAUPA5RTQQ56NN2S3PV5A2FTBX2MDVNMC6SI74FJSO4GGGSAY2TN
+
+	//2021/03/10 07:29:38 Secret key: SAWRQSCT3BLGVSBWZCUB76M4CCSNA6GME4X5JPC36UD6524BBJBJLGCE
+	//2021/03/10 07:29:38 Public key: GDYO22NN6UCRI2TRPFAR4RC4LQFFZU673RF4LTZBFUJDUECG5QVMINHM
 
 	resp, err := http.Get("https://friendbot.stellar.org/?addr=" + address)
 	if err != nil {
