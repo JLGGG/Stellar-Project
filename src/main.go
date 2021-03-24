23d00b185b8a1179969371a9e5276e8a2d526380
Junsu Lim <blockboy2957@gmail.com>
2021-03-24 21:44:11 +0900

    Making account creation function

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stellar/go/keypair"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"log"
 	"time"
@@ -9,12 +11,25 @@ import (
 // Token information is always kept private
 const telegramBotToken string = ""
 
+func makeAccount() (string, string) {
+	pair, err := keypair.Random()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := pair.Address()
+	seed := pair.Seed()
+	log.Printf("Secret key: %s", seed)
+	log.Printf("Public key: %s", address)
+	return address, seed
+}
+
 func main() {
+	keys := make([]string, 2)
 	b, err := tb.NewBot(tb.Settings{
 		Token:  telegramBotToken,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
-
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -22,6 +37,15 @@ func main() {
 
 	b.Handle("/hello", func(m *tb.Message) {
 		b.Send(m.Sender, "Hello World!")
+	})
+
+	// need to add key-value DB for saving keys at this point.
+	b.Handle("/make_account", func(m *tb.Message) {
+		keys[0], keys[1] = makeAccount()
+		address := fmt.Sprintf("Public key: %s", keys[0])
+		seed := fmt.Sprintf("Secret key: %s", keys[1])
+		b.Send(m.Sender, address)
+		b.Send(m.Sender, seed)
 	})
 
 	b.Start()
